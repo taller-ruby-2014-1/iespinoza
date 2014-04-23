67ecc2f729c25495176ebefd518d7f62a29ab2e4
Alumno <alumno@localhost.localdomain>
2014-04-23 17:19:47 -0300

    clase 23 abril

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,46 @@
 class PostsController < ApplicationController
 
 	def new
+		@post = Post.new
 	end
 
 	def create
-		render text: params[:post].inspect
+		@post = Post.new(params[:post].permit(:title, :text))
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
-	def create
-		@post = Post.new(post_params)
-
-		@post.save
-		redirect_to @post
-	end
-		
 	def show
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	def index
-		@posts = Post.all
+		@post = Post.all
 	end
 
 	private
